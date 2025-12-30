4f0247ee7db77883f987788a119455fd7f02bb9c
Qi-Month <dy757410@163.com>
2025-12-31 00:33:06 +0800

    texs and lib

--- src/main/java/top/nebula/cmi/mixin/BasinCategoryMixin.java
+++ src/main/java/top/nebula/cmi/mixin/BasinCategoryMixin.java
@@ -19,14 +19,14 @@ public abstract class BasinCategoryMixin {
 	@Inject(method = "setRecipe(Lmezz/jei/api/gui/builder/IRecipeLayoutBuilder;Lcom/simibubi/create/content/processing/basin/BasinRecipe;Lmezz/jei/api/recipe/IFocusGroup;)V",
 			at = @At(value = "INVOKE", target = "Lcom/simibubi/create/content/processing/basin/BasinRecipe;getRequiredHeat()Lcom/simibubi/create/content/processing/recipe/HeatCondition;"),
 			remap = false, cancellable = true)
-	public void setRecipe(IRecipeLayoutBuilder builder, BasinRecipe recipe, IFocusGroup focuses, CallbackInfo ci) {
+	public void setRecipe(IRecipeLayoutBuilder builder, BasinRecipe recipe, IFocusGroup focuses, CallbackInfo info) {
 		if (recipe.getRequiredHeat() == HeatCondition.valueOf("GRILLED"))
-			ci.cancel();
+			info.cancel();
 	}
 
 	@Inject(method = "draw(Lcom/simibubi/create/content/processing/basin/BasinRecipe;Lmezz/jei/api/gui/ingredient/IRecipeSlotsView;Lnet/minecraft/client/gui/GuiGraphics;DD)V",
 			at = @At("HEAD"), remap = false)
-	public void draw(BasinRecipe recipe, IRecipeSlotsView recipeSlotsView, GuiGraphics graphics, double mouseX, double mouseY, CallbackInfo ci) {
+	public void draw(BasinRecipe recipe, IRecipeSlotsView recipeSlotsView, GuiGraphics graphics, double mouseX, double mouseY, CallbackInfo info) {
 		if (recipe.getRequiredHeat() == HeatCondition.valueOf("GRILLED")) {
 			graphics.drawString(Minecraft.getInstance().font, Lang.translateDirect("cmi.recipe.tip.fire"), 9, 2, recipe.getRequiredHeat().getColor(), false);
 		}

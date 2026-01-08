096e422b6c7c429972d363f07c499cca774ce0bf
Qi-Month <dy757410@163.com>
2026-01-08 22:12:16 +0800

    先push

--- src/main/java/top/nebula/cmi/common/register/ModModifier.java
+++ src/main/java/top/nebula/cmi/common/register/ModModifier.java
@@ -4,17 +4,20 @@ import slimeknights.tconstruct.library.modifiers.util.ModifierDeferredRegister;
 import slimeknights.tconstruct.library.modifiers.util.StaticModifier;
 import top.nebula.cmi.Cmi;
 import top.nebula.cmi.compat.tconstruct.modifier.Acupoint;
+import top.nebula.cmi.compat.tconstruct.modifier.CausalTruncation;
 import top.nebula.cmi.compat.tconstruct.modifier.Frenzy;
 
 public class ModModifier {
 	public static final ModifierDeferredRegister MODIFIERS;
 	public static final StaticModifier<Acupoint> ACUPOINT;
 	public static final StaticModifier<Frenzy> FRENZY;
+	public static final StaticModifier<CausalTruncation> CAUSAL_TRUNCATION;
 
 	static {
 		MODIFIERS = ModifierDeferredRegister.create(Cmi.MODID);
 
 		ACUPOINT = MODIFIERS.register("acupoint", Acupoint::new);
 		FRENZY = MODIFIERS.register("frenzy", Frenzy::new);
+		CAUSAL_TRUNCATION = MODIFIERS.register("causal_truncation", CausalTruncation::new);
 	}
 }

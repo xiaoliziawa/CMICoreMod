a5487d69a36cbae9ceabc1f9413a1b72e59017a8
Qi-Month <dy757410@163.com>
2026-01-30 20:58:24 +0800

    移除冗余类

--- src/main/java/top/nebula/cmi/client/block/resource/CmiBlockPartialModel.java
+++ src/main/java/top/nebula/cmi/client/block/resource/CmiBlockPartialModel.java
@@ -1,16 +1,9 @@
 package top.nebula.cmi.client.block.resource;
 
 import com.jozufozu.flywheel.core.PartialModel;
-import com.simibubi.create.foundation.utility.Couple;
-import net.minecraft.resources.ResourceLocation;
 import top.nebula.cmi.Cmi;
 
-import java.util.HashMap;
-import java.util.Map;
-
 public class CmiBlockPartialModel {
-	public static final Map<ResourceLocation, Couple<PartialModel>> FOLDING_DOORS = new HashMap<>();
-
 	public static final PartialModel STEAM_HAMMER;
 
 	static {

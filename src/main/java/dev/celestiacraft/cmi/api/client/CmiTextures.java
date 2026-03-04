cf7cb8c1f00425aa399ce37a7be5e943c6c8c760
Qi-Month <dy757410@163.com>
2026-03-04 14:32:08 +0800

    换成tex

--- src/main/java/dev/celestiacraft/cmi/api/client/CmiTextures.java
+++ src/main/java/dev/celestiacraft/cmi/api/client/CmiTextures.java
@@ -1,10 +1,16 @@
 package dev.celestiacraft.cmi.api.client;
 
+import com.mojang.blaze3d.systems.RenderSystem;
+import com.simibubi.create.foundation.gui.element.ScreenElement;
 import com.tterrag.registrate.providers.DataGenContext;
 import com.tterrag.registrate.providers.RegistrateItemModelProvider;
 import com.tterrag.registrate.util.nullness.NonNullBiConsumer;
+import dev.celestiacraft.cmi.Cmi;
+import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.Item;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
 
 public class CmiTextures {
 	public static class Items {
@@ -18,6 +24,37 @@ public class CmiTextures {
 			return (context, provider) -> {
 				provider.generated(context::getEntry, provider.modLoc(texture));
 			};
+		}
+	}
+
+	public static class Guis implements ScreenElement {
+		public final ResourceLocation location;
+		public final int width;
+		public final int height;
+		public final int startX;
+		public final int startY;
+
+		public Guis(String path, int width, int height) {
+			this(path, 0, 0, width, height);
+		}
+
+		public Guis(String path, int startX, int startY, int width, int height) {
+			this.location = Cmi.loadResource(String.format("textures/gui/jei/%s.png", path));
+			this.startX = startX;
+			this.startY = startY;
+			this.width = width;
+			this.height = height;
+		}
+
+		@OnlyIn(Dist.CLIENT)
+		public void bind() {
+			RenderSystem.setShaderTexture(0, location);
+		}
+
+		@OnlyIn(Dist.CLIENT)
+		@Override
+		public void render(GuiGraphics graphics, int x, int y) {
+			graphics.blit(location, x, y, startX, startY, width, height);
 		}
 	}
 }
--- src/main/java/dev/celestiacraft/cmi/compat/jei/api/CmiGuiTextures.java
+++ src/main/java/dev/celestiacraft/cmi/compat/jei/api/CmiGuiTextures.java
@@ -1,19 +1,21 @@
 package dev.celestiacraft.cmi.compat.jei.api;
 
+import dev.celestiacraft.cmi.api.client.CmiTextures;
+
 public class CmiGuiTextures {
-    public static final CmiGuiTexture WATER_PUMP_SEA_WATER_ARROW;
-    public static final CmiGuiTexture WATER_PUMP_ARROW;
+	public static final CmiTextures.Guis WATER_PUMP_SEA_WATER_ARROW;
+	public static final CmiTextures.Guis WATER_PUMP_ARROW;
 
-    static {
-        WATER_PUMP_SEA_WATER_ARROW = addGuiTexture("arrows", 1, 1, 63, 24);
-        WATER_PUMP_ARROW = addGuiTexture("arrows", 1, 25, 63, 48);
-    }
+	static {
+		WATER_PUMP_SEA_WATER_ARROW = addGuiTexture("arrows", 1, 1, 63, 24);
+		WATER_PUMP_ARROW = addGuiTexture("arrows", 1, 25, 63, 48);
+	}
 
-    public static CmiGuiTexture addGuiTexture(String path, int startX, int startY, int width, int height) {
-        return new CmiGuiTexture(path, startX, startY, width, height);
-    }
+	public static CmiTextures.Guis addGuiTexture(String path, int startX, int startY, int width, int height) {
+		return new CmiTextures.Guis(path, startX, startY, width, height);
+	}
 
-    public static CmiGuiTexture addGuiTexture(String path, int width, int height) {
-        return new CmiGuiTexture(path, 0, 0, width, height);
-    }
+	public static CmiTextures.Guis addGuiTexture(String path, int width, int height) {
+		return new CmiTextures.Guis(path, 0, 0, width, height);
+	}
 }

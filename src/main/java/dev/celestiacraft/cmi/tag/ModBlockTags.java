38e66304a2791487fc0a5343d6435cbb030482a9
Eternal-Snowstorm <3512546747@qq.com>
2026-03-04 14:58:57 +0800

    blockTag

--- src/main/java/dev/celestiacraft/cmi/tag/ModBlockTags.java
+++ src/main/java/dev/celestiacraft/cmi/tag/ModBlockTags.java
@@ -7,9 +7,12 @@ import dev.celestiacraft.libs.tags.TagsBuilder;
 
 public class ModBlockTags {
 	public static final TagKey<Block> GRILL_SOURCES;
+	public static final TagKey<Block> FREeZING_CATALYST;
 
 	static {
 		GRILL_SOURCES = TagsBuilder.block("grill_sources")
+				.namespace(Cmi.MODID);
+		FREeZING_CATALYST = TagsBuilder.block("freezing_catalyst")
 				.namespace(Cmi.MODID);
 	}
 }

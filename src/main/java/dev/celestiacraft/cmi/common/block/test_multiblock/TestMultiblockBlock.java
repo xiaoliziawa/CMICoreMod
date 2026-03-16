d4172bdf9de6fea9b4cd1ba71818023cd62a5e9e
Qi-Month <dy757410@163.com>
2026-03-16 22:41:21 +0800

    EVENT

--- src/main/java/dev/celestiacraft/cmi/common/block/test_multiblock/TestMultiblockBlock.java
+++ src/main/java/dev/celestiacraft/cmi/common/block/test_multiblock/TestMultiblockBlock.java
@@ -1,17 +1,31 @@
 package dev.celestiacraft.cmi.common.block.test_multiblock;
 
+import com.lowdragmc.lowdraglib.test.TestBlockEntity;
+import com.simibubi.create.AllTags;
 import com.simibubi.create.foundation.block.IBE;
+import dev.celestiacraft.cmi.Cmi;
+import dev.celestiacraft.cmi.common.block.water_pump.WaterPumpBlockEntity;
+import dev.celestiacraft.cmi.common.register.CmiBlock;
 import dev.celestiacraft.cmi.common.register.CmiBlockEntityTypes;
+import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.context.BlockPlaceContext;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.*;
 import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.StateDefinition;
 import net.minecraft.world.level.block.state.properties.BlockStateProperties;
 import net.minecraft.world.level.block.state.properties.DirectionProperty;
+import net.minecraftforge.event.entity.player.PlayerInteractEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
 import org.jetbrains.annotations.NotNull;
 
+@Mod.EventBusSubscriber(modid = Cmi.MODID, bus = Mod.EventBusSubscriber.Bus.FORGE)
 public class TestMultiblockBlock extends Block implements IBE<TestMultiblockBlockEntity> {
 	public static final DirectionProperty FACING = BlockStateProperties.FACING;
 
@@ -45,5 +59,30 @@ public class TestMultiblockBlock extends Block implements IBE<TestMultiblockBloc
 
 	public BlockEntityType<? extends TestMultiblockBlockEntity> getBlockEntityType() {
 		return CmiBlockEntityTypes.TEST_MULTIBLOCK.get();
+	}
+
+	@SubscribeEvent
+	public static void onRightClick(PlayerInteractEvent.RightClickBlock event) {
+		Level level = event.getLevel();
+		BlockPos pos = event.getPos();
+		Player player = event.getEntity();
+		InteractionHand hand = event.getHand();
+		ItemStack item = player.getItemInHand(hand);
+		BlockState state = level.getBlockState(pos);
+
+		if (!level.isClientSide()) {
+			return;
+		}
+
+		boolean isBlockAndItem = state.is(CmiBlock.TEST_MULTIBLOCK.get())
+				&& item.is(AllTags.AllItemTags.WRENCH.tag);
+
+		if (isBlockAndItem && hand == InteractionHand.MAIN_HAND && !player.isShiftKeyDown()) {
+			TestMultiblockBlockEntity entity = (TestMultiblockBlockEntity) level.getBlockEntity(pos);
+			if (entity != null) {
+				player.swing(hand);
+				entity.showMultiblock();
+			}
+		}
 	}
 }

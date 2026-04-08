f99b52a2b1fb6d0304f46712004e715d3d957c3b
xiaoliziawa <3682822069@qq.com>
2026-04-08 14:10:50 +0800

    RecipeType半自动注册

--- src/main/java/dev/celestiacraft/cmi/common/register/CmiRecipeType.java
+++ src/main/java/dev/celestiacraft/cmi/common/register/CmiRecipeType.java
@@ -1,6 +1,7 @@
 package dev.celestiacraft.cmi.common.register;
 
 import dev.celestiacraft.cmi.common.recipe.fluid_burn.FluidBurnRecipe;
+import net.minecraft.world.item.crafting.Recipe;
 import net.minecraft.world.item.crafting.RecipeType;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
@@ -18,22 +19,24 @@ public class CmiRecipeType {
 	public static final Supplier<RecipeType<SpaceElevatorBaseRecipe>> SPACE_ELEVATOR_BASE;
 	public static final Supplier<RecipeType<SpaceElevatorConstructionRecipe>> SPACE_ELEVATOR_CONSTRUCTION;
 	public static final Supplier<RecipeType<FluidBurnRecipe>> FLUID_BURN;
+	public static final Supplier<RecipeType<?>> TEST_COKE_OVEN;
 
 	static {
 		RECIPE_TYPES = DeferredRegister.create(ForgeRegistries.RECIPE_TYPES, Cmi.MODID);
 
-		ACCELERATOR = RECIPE_TYPES.register("accelerator", () -> {
-			return AcceleratorRecipe.Type.INSTANCE;
-		});
-		SPACE_ELEVATOR_BASE = RECIPE_TYPES.register("space_elevator_base", () -> {
-			return SpaceElevatorBaseRecipe.Type.INSTANCE;
-		});
-		SPACE_ELEVATOR_CONSTRUCTION = RECIPE_TYPES.register("space_elevator_construction", () -> {
-			return SpaceElevatorConstructionRecipe.Type.INSTANCE;
-		});
-		FLUID_BURN = RECIPE_TYPES.register("fluid_burn", () -> {
-			return FluidBurnRecipe.Type.INSTANCE;
-		});
+		ACCELERATOR = register("accelerator", () -> AcceleratorRecipe.Type.INSTANCE);
+		SPACE_ELEVATOR_BASE = register("space_elevator_base", () -> SpaceElevatorBaseRecipe.Type.INSTANCE);
+		SPACE_ELEVATOR_CONSTRUCTION = register("space_elevator_construction", () -> SpaceElevatorConstructionRecipe.Type.INSTANCE);
+		FLUID_BURN = register("fluid_burn", () -> FluidBurnRecipe.Type.INSTANCE);
+		TEST_COKE_OVEN = registerSimple("test_coke_oven");
+	}
+
+	private static <T extends Recipe<?>> Supplier<RecipeType<T>> register(String path, Supplier<RecipeType<T>> supplier) {
+		return RECIPE_TYPES.register(path, supplier);
+	}
+
+	private static Supplier<RecipeType<?>> registerSimple(String path) {
+		return RECIPE_TYPES.register(path, () -> RecipeType.simple(Cmi.loadResource(path)));
 	}
 
 	public static void register(IEventBus bus) {

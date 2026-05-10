b71e091e15381b119186f950e617ef38e7ed5bcf
Eternal-Snowstorm <eternalsnowstorm@foxmail.com>
2026-05-10 19:19:53 +0800

    传送带套壳

--- src/main/java/dev/celestiacraft/cmi/event/encase/BeltEncaseHandler.java
+++ src/main/java/dev/celestiacraft/cmi/event/encase/BeltEncaseHandler.java
@@ -1,15 +1,21 @@
 package dev.celestiacraft.cmi.event.encase;
 
-import com.simibubi.create.content.kinetics.belt.BeltBlock;
-import com.simibubi.create.content.kinetics.belt.BeltBlockEntity;
-import com.simibubi.create.content.kinetics.belt.BeltSlope;
+import com.simibubi.create.AllBlocks;
+import com.simibubi.create.AllItems;
+import com.simibubi.create.content.kinetics.belt.*;
+import dev.celestiacraft.cmi.Cmi;
+import dev.celestiacraft.cmi.common.register.CmiMechanism;
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.InteractionHand;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraftforge.event.entity.player.PlayerInteractEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
 
+@Mod.EventBusSubscriber(modid = Cmi.MODID, bus = Mod.EventBusSubscriber.Bus.FORGE)
 public class BeltEncaseHandler {
 
 	@SubscribeEvent
@@ -26,15 +32,56 @@ public class BeltEncaseHandler {
 		if (player.isShiftKeyDown()) {
 			return;
 		}
+		if (!player.getItemInHand(InteractionHand.OFF_HAND).is(CmiMechanism.ANDESITE.get())) {
+			return;
+		}
 
 		ItemStack stack = event.getItemStack();
-		BlockState state = level.getBlockState(event.getPos());
-		Object part = state.getValue(BeltBlock.PART);
-		Object horizontalFacing = state.getValue(BeltBlock.HORIZONTAL_FACING);
-		Object slope = state.getValue(BeltBlock.SLOPE);
-		boolean yBelt = slope == BeltSlope.VERTICAL;
-		boolean yAxis = slope != BeltSlope.HORIZONTAL;
-		Object beltCasingType = BeltBlockEntity.CasingType.NONE;
-
+		BlockPos pos = event.getPos();
+		BlockState state = level.getBlockState(pos);
+		BeltBlockEntity controller = BeltHelper.getControllerBE(level, pos);
+		int beltLength = controller.beltLength;
+		if (state.getValue(BeltBlock.SLOPE) == BeltSlope.VERTICAL) {
+			BlockPos beltStart = pos;
+			for (int i = 0; i < 20; i++) {
+				BlockState beltState = level.getBlockState(beltStart);
+				if (beltState.getValue(BeltBlock.PART) == BeltPart.START || beltState.getValue(BeltBlock.PART) == BeltPart.END) {
+					break;
+				}
+				beltStart = beltStart.below();
+			}
+			for (int i = 0; i < beltLength; i++) {
+				BeltBlockEntity targetEntity = BeltHelper.getSegmentBE(level, beltStart.above(i));
+				if (targetEntity == null) {
+					return;
+				}
+				if (stack.is(AllBlocks.ANDESITE_CASING.asItem())) {
+					targetEntity.setCasingType(BeltBlockEntity.CasingType.ANDESITE);
+				} else if (stack.is(AllBlocks.BRASS_CASING.asItem())) {
+					targetEntity.setCasingType(BeltBlockEntity.CasingType.BRASS);
+				} else if (stack.is(AllItems.WRENCH.get())) {
+					event.setCanceled(true);
+					player.swing(InteractionHand.MAIN_HAND);
+					targetEntity.setCasingType(BeltBlockEntity.CasingType.NONE);
+				}
+			}
+		} else {
+			for (int i = 0; i < beltLength; i++) {
+				BlockPos beltBlock = BeltHelper.getPositionForOffset(controller, i);
+				BeltBlockEntity targetEntity = BeltHelper.getSegmentBE(level, beltBlock);
+				if (targetEntity == null) {
+					return;
+				}
+				if (stack.is(AllBlocks.ANDESITE_CASING.asItem())) {
+					targetEntity.setCasingType(BeltBlockEntity.CasingType.ANDESITE);
+				} else if (stack.is(AllBlocks.BRASS_CASING.asItem())) {
+					targetEntity.setCasingType(BeltBlockEntity.CasingType.BRASS);
+				} else if (stack.is(AllItems.WRENCH.get())) {
+					event.setCanceled(true);
+					player.swing(InteractionHand.MAIN_HAND);
+					targetEntity.setCasingType(BeltBlockEntity.CasingType.NONE);
+				}
+			}
+		}
 	}
 }

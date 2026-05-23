7c95c6507bfc323fc28b87b3272099341a28a09d
Qi-Month <dy757410@163.com>
2026-05-23 17:07:48 +0800

    GetDateModifyTitle

--- src/main/java/dev/celestiacraft/cmi/event/GetDateModifyTitle.java
+++ src/main/java/dev/celestiacraft/cmi/event/GetDateModifyTitle.java
@@ -1,31 +1,32 @@
 package dev.celestiacraft.cmi.event;
 
+import dev.celestiacraft.libs.NebulaLibs;
 import dev.celestiacraft.libs.utils.FestivalUtils;
-import net.minecraft.client.Minecraft;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 
-import java.time.LocalDateTime;
-
 @Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
 public class GetDateModifyTitle {
-	private static void modifyTitle(String title) {
-		Minecraft.getInstance().getWindow().setTitle(title);
-	}
-
 	@SubscribeEvent
 	public static void onClientSetup(FMLClientSetupEvent event) {
-		int month = LocalDateTime.now().getMonthValue();
-		int day = LocalDateTime.now().getDayOfMonth();
-
 		event.enqueueWork(() -> {
+			String title;
+
 			if (FestivalUtils.isAprilFoolsDay()) {
-				modifyTitle("Create: Infinity Mechanism");
+				title = "Create: Infinity Mechanism";
 			} else {
-				modifyTitle("Create: Mechanism and Innovation");
+				title = "Create: Mechanism and Innovation";
 			}
+
+			String greeting = FestivalUtils.getFestivalGreeting();
+
+			if (greeting != null) {
+				title += " - " + greeting;
+			}
+
+			NebulaLibs.modifyWindowsTitle(title);
 		});
 	}
 }

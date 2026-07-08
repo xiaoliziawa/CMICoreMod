36ede8b1a1ef5cedc1369ea6b7512466e31e7073
Qi-Month <dy757410@163.com>
2026-07-08 20:54:50 +0800

    tag

--- src/main/java/dev/celestiacraft/cmi/compat/kubejs/custom/item/CdgHammerItemBuilder.java
+++ src/main/java/dev/celestiacraft/cmi/compat/kubejs/custom/item/CdgHammerItemBuilder.java
@@ -1,5 +1,6 @@
 package dev.celestiacraft.cmi.compat.kubejs.custom.item;
 
+import com.jesz.createdieselgenerators.CreateDieselGenerators;
 import com.jesz.createdieselgenerators.content.tools.hammer.HammerItem;
 import dev.latvian.mods.kubejs.item.custom.HandheldItemBuilder;
 import net.minecraft.resources.ResourceLocation;
@@ -8,6 +9,7 @@ import net.minecraft.world.item.Item;
 public class CdgHammerItemBuilder extends HandheldItemBuilder {
 	public CdgHammerItemBuilder(ResourceLocation location) {
 		super(location, 0.0F, -2.0F);
+		tag(CreateDieselGenerators.rl("hammers"));
 	}
 
 	@Override
